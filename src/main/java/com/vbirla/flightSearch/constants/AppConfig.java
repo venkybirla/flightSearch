122ab3de8b22fc646d71f6c8c92d071493bdd8af
Venktesh Birla <Venktesh.Birla@AASPL-BRD.COM>
2020-12-28 14:41:42 +0530

    Handling time difference between flights.

--- src/main/java/com/vbirla/flightSearch/constants/AppConfig.java
+++ src/main/java/com/vbirla/flightSearch/constants/AppConfig.java
@@ -3,4 +3,5 @@ package com.vbirla.flightSearch.constants;
 public interface AppConfig {
 
 	Integer MAX_HOPES =2;
+	Integer MAX_INTERVAL_TIME_BETWEEN_HOPES = 120;
 }
--- src/main/java/com/vbirla/flightSearch/service/impl/FlightSearchServiceImpl.java
+++ src/main/java/com/vbirla/flightSearch/service/impl/FlightSearchServiceImpl.java
@@ -8,6 +8,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import com.vbirla.flightSearch.constants.AppConfig;
 import com.vbirla.flightSearch.model.CityConnection;
@@ -29,10 +30,12 @@ public class FlightSearchServiceImpl implements FlightSearchService{
 	public SearchResult getSearchResult(String from, String to, String departureDate) {
 		RouteGraphInfoByDate routeGraphInfoByDate = routeGraphInfoByDateRepository.findRouteGraphInfoByDateById(departureDate);
 		SearchResult searchResult  = new SearchResult();
-		List<Transport> transportList = new ArrayList<Transport>();
-		Map<String, Boolean> isVisitedMap = new HashMap<String, Boolean>();
-		getAllPaths(from, to, isVisitedMap, transportList, routeGraphInfoByDate, 0, searchResult);
-		Collections.sort(searchResult.getRouteList(), new Route.SortRouteByFare());
+		if(routeGraphInfoByDate!=null) {
+			List<Transport> transportList = new ArrayList<Transport>();
+			Map<String, Boolean> isVisitedMap = new HashMap<String, Boolean>();
+			getAllPaths(from, to, isVisitedMap, transportList, routeGraphInfoByDate, 0, searchResult);
+			Collections.sort(searchResult.getRouteList(), new Route.SortRouteByFare());
+		}
 		return searchResult;
 	}
 	
@@ -49,28 +52,50 @@ public class FlightSearchServiceImpl implements FlightSearchService{
 		
 		for(Map.Entry<String, List<Transport>> entry: transportListByDestination.entrySet()) {
 			for(Transport transport : entry.getValue()) {
-				transportList.add(transport);
-				if(entry.getKey().equals(to)) {
-					Route route = new Route(); 
-					long totalFare = 0;
-					for(Transport trans : transportList) {
-						route.getTransportList().add(trans);
-						totalFare = totalFare+trans.getFare();
+				if(transportList.isEmpty() || isValidTransport(transportList.get(hope-1), transport)){
+					transportList.add(transport);
+					if(entry.getKey().equals(to)) {
+						Route route = new Route(); 
+						long totalFare = 0;
+						for(Transport trans : transportList) {
+							route.getTransportList().add(trans);
+							totalFare = totalFare+trans.getFare();
+						}
+						route.setFare(totalFare);
+						searchResult.getRouteList().add(route);
+						if(searchResult.getFrom()==null) {
+							searchResult.setFrom(transportList.get(0).getFrom());
+							searchResult.setTo(transport.getTo());
+						}
+					}else {
+						getAllPaths(entry.getKey(), to, isVisitedMap, transportList, routeGraphInfoByDate, hope+1, searchResult);
 					}
-					route.setFare(totalFare);
-					searchResult.getRouteList().add(route);
-					if(searchResult.getFrom()==null) {
-						searchResult.setFrom(transportList.get(0).getFrom());
-						searchResult.setTo(transport.getTo());
-					}
-				}else {
-					getAllPaths(entry.getKey(), to, isVisitedMap, transportList, routeGraphInfoByDate, hope+1, searchResult);
+					transportList.remove(hope);
 				}
-				transportList.remove(hope);
 			}
 		}
 		
 		isVisitedMap.put(from , false);
 	} 
+	
+	private int convertdepartTimeinMinutes(String time) {
+		int convertedTime = 0;
+		if(StringUtils.hasText(time)) {
+			String[] split = time.split(":");
+			convertedTime = Integer.parseInt(split[0])*60+Integer.parseInt(split[1]);
+		}
+		return convertedTime;
+	}
+	
+	private boolean isValidTransport(Transport prvious, Transport next) {
+		boolean isValidTransport = false;
+		int previousTransportArrival = convertdepartTimeinMinutes(prvious.getTime())+prvious.getDuration();
+		int nextTransportDepature = convertdepartTimeinMinutes(next.getTime());
+		int timeDiff = nextTransportDepature-previousTransportArrival;
+		if(timeDiff>=0 && timeDiff<=AppConfig.MAX_INTERVAL_TIME_BETWEEN_HOPES) {
+			isValidTransport = true;
+		}
+		return isValidTransport;
+	}
 
 }
